81c25d22413d8d5ba926a2561e8ddb5a80b17632
Hubert Lipiński <hubertos29@gmail.com>
2019-11-07 21:25:06 +0100

    navbar structure

---
+++ resources/views/layouts/navbar/topbar.blade.php
@@ -0,0 +1,40 @@
+<div class="top_bar">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex flex-row">
+                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="images/phone.png" alt=""></div>+48 123 456 789</div>
+                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="images/mail.png" alt=""></div><a href="mailto:hubertlipinskipl@gmail.com">hubertlipinskipl@gmail.com</a></div>
+                <div class="top_bar_contact_item m-auto text-danger text-uppercase">
+                    This is demo version
+                </div>
+                <div class="top_bar_content ml-auto">
+                    <div class="top_bar_menu">
+                        <ul class="standard_dropdown top_bar_dropdown">
+                            <li>
+                                <a href="#">English<i class="fas fa-chevron-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Polish</a></li>
+                                    <li><a href="#" class="text-muted">Spanish</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">$ US dollar<i class="fas fa-chevron-down"></i></a>
+                                <ul>
+                                    <li><a href="#">PLN Polish złoty</a></li>
+                                    <li><a href="#">EUR Euro</a></li>
+                                    <li><a href="#">GBP British Pound</a></li>
+                                    <li><a href="#">JPY Japanese Yen</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="top_bar_user">
+                        <div class="user_icon"><img src="{{asset('images/test.png')}}" alt=""></div>
+                        <div><a href="#">Register</a></div>
+                        <div><a href="{{ route('login') }}">{{ __('Login') }}</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
