073e3789e8bae981cb0df791c454ff9b1ac7180a
HubertLipinski <hubertlipinskipl@gmail.com>
2020-02-23 15:06:20 +0100

    added new sections in cart view

--- resources/views/layouts/cart/cart.blade.php
+++ resources/views/layouts/cart/cart.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <p class="h1">Twój koszyk</p>
         <div class="container py-2">
+            <p class="h3 pt-2">1. Przedmioty</p>
             <table class="table table-borderless table-hover text-center">
                 <thead>
                     <tr>
@@ -36,15 +37,82 @@
                     </td>
                 </tr>
                 </tbody>
-                <hr/>
             </table>
-            <hr/>
             <div>
-                form z addresem
+                <p class="h3 pt-2">2. Adres dostawy</p>
+                <div class="row px-3 py-2">
+                    <div class="col-md-2 d-flex align-items-center justify-content-center">
+                        <div class="form-group w-100">
+                            <label for="shipping-address">Zapisane adresy</label>
+                            <select class="form-control" id="shipping-address">
+                                <option>Domowy</option>
+                                <option>Dom 2</option>
+                                <option>Poczta</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-10 d-flex justify-content-center">
+                        <div class="cart-saved-address ">
+                            <p><b>Imie: </b> Jan </p>
+                            <p><b>Nazwisko: </b> Nowak</p>
+                            <p><b>Adres: </b> Tymczasowa 1 </p>
+                            <p><b>Kod pocztowy: </b> 55-032 </p>
+                            <p><b>Miasto: </b> Poznań</p>
+                            <p><b>Kraj: </b> Polska</p>
+                            <p><b>Nr. Telefonu: </b> +482300101092 </p>
+                        </div>
+                        <div class="w-75 d-none">
+                            <form>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputEmail4">Imie</label>
+                                        <input type="text" class="form-control" id="inputEmail4">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputPassword4">Nazwisko</label>
+                                        <input type="text" class="form-control" id="inputPassword4">
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-8">
+                                        <label for="inputAddress">Adres</label>
+                                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="inputPassword4">Nr Telefonu</label>
+                                        <input type="text" class="form-control" id="inputPassword4">
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputCity">Miasto</label>
+                                        <input type="text" class="form-control" id="inputCity">
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="inputState">Województwo</label>
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose...</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="inputZip">Kod pocztowy</label>
+                                        <input type="text" class="form-control" id="inputZip">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="w-100 p-5 text-right">
-                <p class="h5">Do zapłaty: 123 zł</p>
-                kontynuuj
+            <div>
+                <p class="h3 pt-2">3. Płatność</p>
+                kafelki wybierane
+            </div>
+            <div class="p-5 text-right">
+                <p class="h4">Do zapłaty: <b>123 zł</b></p>
+{{--                <p class="text-muted">Kupon rabatowy -</p>--}}
+                <a href="#" role="button" class="btn btn-lg btn-outline-primary py-2">Przejdź do płatności</a>
             </div>
         </div>
     </div>
