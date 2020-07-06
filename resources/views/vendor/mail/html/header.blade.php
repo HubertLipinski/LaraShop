292c470ab90364f5d77a2d8b7701893e7884ce05
HubertLipinski <hubertlipinskipl@gmail.com>
2020-07-06 11:04:50 +0200

    Customized email template, created OrderPlaced email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <img src="{{asset('img/logo_blue.png')}}" alt="Larashop - best web store" style="transform: scale(0.7)">
+        </a>
+    </td>
+</tr>
